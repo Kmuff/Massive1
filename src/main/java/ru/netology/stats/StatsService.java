6bf73bf75d4a0743f711890b5930580ad594e1d9
Neto test <munkey@list.ru>
2023-05-29 17:03:46 +0300

    Origin master

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,67 @@
+package ru.netology.stats;
+public class StatsService {
+    public long sum(long[] sales) {
+        long allSale = 0;
+        for (long sale : sales) {
+            allSale += sale;
+
+        }
+        return allSale;
+    }
+
+    public long average(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+        long averageSale = totalSale / 12;
+        return averageSale;
+    }
+
+    public int mothMaximumSale(long[] sales) {
+        int monthMaximum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[monthMaximum]) {
+                monthMaximum = i;
+            }
+        }
+        return monthMaximum + 1;
+    }
+
+    public int mothMinimumSale(long[] sales) {
+        int monthMinimum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[monthMinimum]) {
+                monthMinimum = i;
+            }
+        }
+        return monthMinimum + 1;
+    }
+
+
+    public int salesBelowAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int salesAboveAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale > average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
+
+
+
+
+
+
